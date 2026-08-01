e7ad05b796bd7bb5780af98fbb1c26d4bde82e83
osintcss <187201470+osintcss@users.noreply.github.com>
2026-07-31 20:51:23 -0400

    Fix admin authorization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
         $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
             'maintenance' => \App\Http\Middleware\CheckMaintenanceMode::class,
         ]);
     })
